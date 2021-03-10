3035c00725c21d5c6a570c7d43dfc5a8914a4cde
Angger Bayu Pangestu <kontak@angger.me>
2021-03-10 17:42:31 +0700

    Menambahkan script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+const restaurant = {
+  name: "Rumah makan berkah",
+  Lokasi: "Jalan merpati blok m",
+  Kategori: "tradisional, rempah, gurih, lezar",
+  menuPembuka: "Sate, eskrim, soto",
+  menuUtama: "Padang, gulai, rendang",
+};
